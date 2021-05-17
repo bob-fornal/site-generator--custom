c9211b656dea6756207bfffa0379ba7713cbf0e9
Bob Fornal <bob@fornal.org>
2021-05-17 10:07:06 -0400

    Futher Abstraction and Logging

--- classes/build-tool-class.js
+++ classes/build-tool-class.js
@@ -2,7 +2,11 @@
 import fs from 'fs';
 import path from 'path';
 
+import fse from 'fs-extra';
+
 export default class BuildToolClass {
+
+  output = false;
 
   srcString = '';
   distString = '';
@@ -16,10 +20,12 @@ export default class BuildToolClass {
   files = {};
 
   constructor(
+    output = true,
     src = 'src',
     dist = 'dist',
     templates = 'templates'
   ) {
+    this.output = output;
     this.srcString = src;
     this.distString = dist;
     this.templateString = templates;
@@ -34,12 +40,45 @@ export default class BuildToolClass {
   readFile = (to, filename) => fs.readFileSync(path.join(to, filename), 'utf8');
   saveFile = (to, filename, contents) => fs.writeFileSync(path.join(to, filename), contents);
   
-  templatize = (content, templates) => {
+  templatize = (content, templates, matchFn) => {
     for (let key in templates) {
       const match = `<!-- TEMPLATE: ${ key } -->`;
       content = content.replace(match, templates[key]);
     }
     return content;
+  };
+
+  clearDistributionDirectory = (path) => {
+    this.output && console.log(`Processing ${ path } Directory`);
+    if (fs.existsSync(path)) {
+      fs.rmSync(path, { recursive: true });
+    }
+    fs.mkdirSync(path);
+  };
+
+  getAndReadFiles = (path, pattern = '') => {
+    let result = {};
+    const files = this.readFilenamesFromDirectory(path);
+    files.forEach(file => {
+      if ((pattern.length === 0) || (file.substring(file.length - pattern.length) === pattern)) {
+        result[file] = this.readFile(path, file);
+        this.output && console.log(`Reading File: ${ file }`);
+      }
+    });
+    return result;
+  };
+
+  copyStaticFiles = (title, folder) => {
+    this.output && console.log(`Copying ${ title } Files`);
+    const from = path.join(this.srcPath, folder);
+    const to = path.join(this.distPath, folder);
+    fse.copySync(from, to, { overwrite: true }, (error) => {
+      if (error) {
+        throw error;
+      } else {
+        this.output && console.log(`Moved ${ title } Files`);
+      }
+    });
   };
     
   process = () => {  
--- index.js
+++ index.js
@@ -1,72 +1,39 @@
 
-import fs from 'fs';
-import path from 'path';
-import fse from 'fs-extra';
-
 import BuildToolClass from './classes/build-tool-class.js';
 
 class BuildTool extends BuildToolClass {
 
-  // Implemented Tasks
-  clearSrcDirectory = () => {
-    console.log('Removing DIST folder');
-    if (fs.existsSync(this.distPath)) {
-      fs.rmSync(this.distPath, { recursive: true });
-    }
-    fs.mkdirSync(this.distPath);
-  };
-
+  // Implementation Tasks
   getTemplates = () => {
-    this.templates = {};
-    const templateFiles = this.readFilenamesFromDirectory(this.templatePath);
-    templateFiles.forEach(file => {
-      console.log(`Reading Template: ${ file }`);
-      this.templates[file] = this.readFile(this.templatePath, file);
-    });  
+    this.templates = this.getAndReadFiles(this.templatePath, '.template.html');
   };
 
   getHTMLFiles = () => {
-    this.files = {};
-    const workingFiles = this.readFilenamesFromDirectory(this.srcPath);
-    workingFiles.forEach(file => {
-      if (file.substring(file.length - 5) === '.html') {
-        console.log(`Reading File: ${ file }`);
-        this.files[file] = this.readFile(this.srcPath, file);
-      }
-    });
+    this.files = this.getAndReadFiles(this.srcPath, '.html');
   };
 
   processHTMLFiles = () => {
+    const template = (key) => `<!-- TEMPLATE: ${ key } -->`;
+
     for (let key in this.files) {
-      console.log(`Processing File: ${ key }`);
-      const contents = this.templatize(this.files[key], this.templates);
+      this.output && console.log(`Processing File: ${ key } (template)`);
+      const contents = this.templatize(this.files[key], this.templates, template);
       this.saveFile(this.distPath, key, contents);
     }
   };
-
-  processStaticFiles = (title, folder) => {
-    console.log(`Moving ${ title } Files`);
-    const from = path.join(this.srcPath, folder);
-    const to = path.join(this.distPath, folder);
-    fse.copySync(from, to, { overwrite: true }, (error) => {
-      if (error) {
-        throw error;
-      } else {
-        console.log(`Moved ${ title } Files`);
-      }
-    });
-  };
   
   process = () => {  
-    this.clearSrcDirectory();
+    this.clearDistributionDirectory(this.distPath);
+
     this.getTemplates();
     this.getHTMLFiles();
     this.processHTMLFiles();
-    this.processStaticFiles('JS', 'js');
-    this.processStaticFiles('CSS', 'styles');
-    this.processStaticFiles('Assets', 'assets');
+
+    this.copyStaticFiles('JS', 'js');
+    this.copyStaticFiles('CSS', 'styles');
+    this.copyStaticFiles('Assets', 'assets');
   
-    console.log('done.');
+    this.output && console.log('done.');
   };
   
 }
